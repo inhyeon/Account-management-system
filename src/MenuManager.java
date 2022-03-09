60b5482e6afc28fbc310290767346f7def18fc21
inhyeon <inh1216@naver.com>
2022-03-09 20:15:13 +0900

    removed Hello.java and added MenuManager.java

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -2,7 +2,11 @@
 public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("create new account");
+		System.out.println("my accounts");
+		System.out.println("income");
+		System.out.println("expense");
+		System.out.println("exit");
 	
 
 	}
